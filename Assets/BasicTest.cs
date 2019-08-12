0c44e64bb497f479c5981c9a3aaefc4957634606
Will <will.herald.snyder@gmail.com>
2019-08-12 13:50:09 -0400

    physics not going so well

--- Assets/BasicTest.cs
+++ Assets/BasicTest.cs
@@ -7,7 +7,7 @@ public class BasicTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody>().velocity = 5 * transform.forward;   
     }
 
     // Update is called once per frame
